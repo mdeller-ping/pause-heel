42f5d9579c5e796712e670b405eb9fe4575a9982
mdeller-ping <mdeller@pingidentity.com>
2021-01-05 14:32:05 -0500

    remove link

--- dealer/index.php
+++ dealer/index.php
@@ -28,9 +28,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/">Member Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/detail/">Details</a>
-                </li>
             </ul>
             <ul class="navbar-nav text-right mt-4">
                 <li class="nav-item">
